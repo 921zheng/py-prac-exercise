28b592338af97553547e4626ed4621d4bb6cc884
Sophie Zheng <139349595+921zheng@users.noreply.github.com>
2023-12-12 16:16:35 +0000

    Add files via upload

---
+++ py-prac.py
@@ -0,0 +1,123 @@
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+
+def xo(s):
+    
+    
+    return s.lower().count('x') == s.lower().count('o')
+    
+# In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+# The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+# Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+# You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+# The string has a length greater or equal to one and contains only letters from ato z.
+
+# Examples:
+# s="aaabbbbhaijjjm"
+# printer_error(s) => "0/14"
+
+# s="aaaxbbbbyyhwawiwjjjwwm"
+# printer_error(s) => "8/22"
+
+def printer_error(s):
+    list=["a","b","c","d","e","f","g","h","i","j","k","l","m"]
+    count=0
+    for alphebet in s:
+        if alphebet not in list:
+            count+=1
+    return f"{count}/{len(s)}"
+    
+    
+# Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(a1, a2):
+    # your code
+    a=a1+a2
+    result=""
+    
+    for i in a:
+        if i not in result:
+            result+=i
+            
+    return ''.join(sorted(result))
+
+
+
+# DESCRIPTION:
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+# Example
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+def filter_list(l):
+    new_list =[]
+    for x in l:
+        if type(x) != str:
+            new_list.append(x)
+    return new_list
+    
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+# Examples
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+def find_it(seq):
+    for a in seq:
+        if seq.count(a)%2==1:
+            return a
+
+# Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+# For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+# With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+
+def delete_nth(order,max):
+    result=[]
+    for a in order:
+        if result.count(a)<max:
+            result.append(a)
+            continue
+    return result
+
+
+# Rules
+#  1.  The input string will always be lower case but maybe empty.
+
+#  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+# Example
+# wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+def wave(people):
+    return[people[:i] + people[i].upper() + people[i+1:]for i in range(len(people)) if people[i]!=" "]
+
+
+
+
